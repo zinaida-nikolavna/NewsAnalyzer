270c3f02730c066388728f00028f858a8e2b280b
Zina Burova <zina270994@yandex.ru>
2020-06-12 23:49:11 +0300

    сделана сборка проекта webpack

---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/js/main.js',
+    about: './src/js/about.js',
+    analytics: './src/js/analytics.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[chunkhash].js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: { loader: "babel-loader" },
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
+      }
+    },
+    {
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+    },
+    {
+      test: /\.(png|jpg|gif|ico|svg)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            publicPath: 'img',
+            outputPath: 'img',
+            useRelativePath: true,
+            esModule: false,
+          }
+        },
+        {
+          loader: 'image-webpack-loader',
+          options: {}
+        },
+      ]
+    },
+    {
+      test: /\.(eot|ttf|woff|woff2)$/,
+      loader: 'file-loader?name=./vendor/[name].[ext]'
+    }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/[name].[contenthash].css'
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/html/main.html',
+      filename: 'index.html',
+      chunks: ['main']
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/html/about.html',
+      filename: 'about.html',
+      chunks: ['about']
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/html/analytics.html',
+      filename: 'analytics.html',
+      chunks: ['analytics']
+    }),
+    new WebpackMd5Hash()
+  ]
+};
