72ab57fc27a1d07cc1682176f8244a16fc1d6710
Zina Burova <zina270994@yandex.ru>
2020-06-17 20:49:07 +0300

    поправлена сборка deploy

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,14 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
   entry: {
-    main: './src/js/main.js',
-    about: './src/js/about.js',
-    analytics: './src/js/analytics.js'
+    main: './src/index.js',
+    about: './src/about/index.js',
+    analytics: './src/analytics/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -25,7 +27,13 @@ module.exports = {
     },
     {
       test: /\.css$/,
-      use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1
+        },
+      }, 'postcss-loader']
     },
     {
       test: /\.(png|jpg|gif|ico|svg)$/,
@@ -34,8 +42,8 @@ module.exports = {
           loader: 'file-loader',
           options: {
             name: '[name].[ext]',
-            publicPath: '../src/images',
-            outputPath: '../src/images',
+            publicPath: 'images',
+            outputPath: 'images',
             useRelativePath: true,
             esModule: false,
           }
@@ -48,13 +56,16 @@ module.exports = {
     },
     {
       test: /\.(eot|ttf|woff|woff2)$/,
-      loader: 'file-loader?name=../src/vendor/fonts/[name].[ext]'
+      loader: 'file-loader?name=./vendor/fonts/[name].[ext]'
     }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: './css/[name].[contenthash].css'
+      filename: 'css/[name].[contenthash].css'
+    }),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.css$/g,
